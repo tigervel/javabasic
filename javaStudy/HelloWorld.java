f4e112c47cfa8ec321d8f998a604d00185971f28
tigervel <silpy.jdh5057@gmail.com>
2025-05-12 17:44:27 +0900

    init commit

---
+++ javaStudy/HelloWorld.java
@@ -0,0 +1,13 @@
+package javaStudy;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요 주덕호 입니다.");
+		
+//문자셋 설정 : 이 설정이 각각 틀리면, 한글 데이터나, 주석이 깨질수 가 있음.
+//때문에 반드시 문자셋 설정을 초기에 해둬야 나중에 후회하지 않음.
+		//모든 문자셋은 UTF-8 로 설정함.
+	}
+
+}
